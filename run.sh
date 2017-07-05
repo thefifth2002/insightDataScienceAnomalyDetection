242873fa23e41da459047cd9722cfac9d595c1e1
hao wu <thefifth2002@hotmail.com>
2017-07-05 10:54:21 -0400

    missing s

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar AnomalyDetection.jar log_input/batch_log.json log_input/stream_log.json log_output/flagged_purchase.json
+java -jar AnomalyDetection.jar log_input/batch_log.json log_input/stream_log.json log_output/flagged_purchases.json
